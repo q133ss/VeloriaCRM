5200c5cbdfb3d0874eef914cbcf591657d751cdf
Alexey <blockstar2k@gmail.com>
2025-09-14 05:03:25 +0300

    Settings page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -290,17 +290,7 @@
             <div class="content-wrapper">
                 <!-- Content -->
                 <div class="container-xxl flex-grow-1 container-p-y">
-                    <h4 class="py-4 mb-6">Page 1</h4>
-                    <p>
-                        Sample page.<br />For more layout options use
-
-                        <a
-                            href="https://demos.pixinvent.com/materialize-html-admin-template/documentation/layouts.html"
-                            target="_blank"
-                            class="fw-medium"
-                        >Layout docs</a
-                        >.
-                    </p>
+                    @yield('content')
                 </div>
                 <!-- / Content -->
 
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::view('/login', 'auth.login')->name('login');
 Route::view('/register', 'auth.register')->name('register.form');
 Route::view('/forgot-password', 'auth.forgot')->name('password.request');
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('token.cookie')->group(function () {
     Route::view('/dashboard', 'dashboard')->name('dashboard');
     Route::view('/profile', 'profile')->name('profile');
     Route::view('/settings', 'settings')->name('settings');
