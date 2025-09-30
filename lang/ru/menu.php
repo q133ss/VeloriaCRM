450acc68072e693350eb8505a5e5823eadf3cae1
Алексей <84626093+q133ss@users.noreply.github.com>
2025-09-30 19:45:55 +0300

    Localize application menu

---
+++ lang/ru/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'dashboard' => 'Дашборд',
+    'calendar' => 'Календарь',
+    'orders' => 'Записи',
+    'clients' => 'Клиенты',
+    'services' => 'Услуги',
+    'invoices' => 'Инвойсы',
+    'messages' => 'Сообщения',
+    'analytics' => 'Аналитика',
+    'integrations' => 'Интеграции',
+    'settings' => 'Настройки',
+    'help' => 'Помощь',
+    'marketing' => 'Маркетинг',
+    'learning' => 'Обучение',
+    'veloryStudio' => 'Velory Studio',
+    'automations' => 'Автоматизации',
+];
